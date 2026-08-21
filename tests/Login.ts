499364b28512c33d0cfa60e3d3a542578b9532f8
Rahman2Sazal <saz99man.99@gmail.com>
2026-08-21 21:36:01 +0200

    generate readable playwright test report

--- tests/Login.ts
+++ tests/Login.ts
@@ -1,13 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://github.com/');
+
+
+  await test.step('Login with invalid credentials', async () => {
+      await page.goto('https://github.com/');
+  });
+
+ await test.step('Clickand enter credentials', async () => {
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.getByRole('textbox', { name: /Username or email address/i }).fill('invalid-automation-user');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('automationtest');
-  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
-  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+ });
+
+ await test.step('Verify error message', async () => {
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.')
+ });
   await page.pause();
 });
 
--- tests/LoginWithKeyboard.ts
+++ tests/LoginWithKeyboard.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Login with invalid credentials', async ({ page }) => {
   await page.goto('https://github.com/');
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.getByRole('textbox', { name: 'Username or email address' }).click();
