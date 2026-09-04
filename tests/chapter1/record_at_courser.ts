bdebc8a34083bf138ba60220dc5e33f4bcd02cb1
Rahman2Sazal <saz99man.99@gmail.com>
2026-09-04 12:53:52 +0200

    record at courser demo

---
+++ tests/chapter1/record_at_courser.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.youtube.com/');
+  await page.getByRole('button', { name: 'Reject the use of cookies and' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
+  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+  await page.goto('https://www.youtube.com/watch?v=2poXBtifpzA&list=PLUeDIlio4THEgPRVJRqZRS8uw8hhVNQCM');
+});
