72ec777f9564602a0510d3a979fe8da22635014b
Rahman2Sazal <saz99man.99@gmail.com>
2026-09-04 14:55:31 +0200

    record at cursor done

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -1,5 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  // Recording...
+  await page.goto('https://www.youtube.com/');
+  await page.getByRole('button', { name: 'Reject the use of cookies and' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('');
+  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+  
+  await page.goto('https://www.youtube.com/watch?v=2poXBtifpzA&list=PLUeDIlio4THEgPRVJRqZRS8uw8hhVNQCM');
+  // Validate the webpage title
+    await expect(page).toHaveTitle('Playwright by Testers Talk ✅ - YouTube');
+
+  await page.getByRole('link', { name: 'View full playlist' }).first().click();
+
+  await expect(page.getByRole('link', { name: '#1 Playwright Tutorial Full' })).toBeVisible();
+  await expect(page.getByRole('link', { name: '#2 Playwright API Testing' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Testing Microsoft D365 CRM' })).toBeVisible();
+  await expect(page.getByRole('link', { name: '#3 Playwright with Azure' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Playwright #1 Chapter01' })).toBeVisible();
+  await page.getByRole('link', { name: 'Playwright #1 Chapter01' }).click();
+
+  
 });
