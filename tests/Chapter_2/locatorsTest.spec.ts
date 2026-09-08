12b942a25e354f2f91fab7c734bf09215724fc78
Rahman2Sazal <saz99man.99@gmail.com>
2026-09-08 16:16:33 +0200

    locators

--- tests/Chapter_2/locatorsTest.spec.ts
+++ tests/Chapter_2/locatorsTest.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('Locators in Playwright', async ({ page }) => {
-  await page.goto('https://www.youtube.com/');
   
- 
+    // go to url
+    await page.goto('https://www.github.com/BakkappaN/');
 
+    //getByRole
+    await expect(page.getByRole('link', { name: 'Sign in' })).toBeVisible();
+    await page.getByRole('link', { name: 'Sign in' }).click();
+
+    //getByLabel
+    await expect(page.getByLabel('Homepage', {exact : true})).toBeVisible();
+    await page.getByLabel('Homepage', { exact: true }).first().click();
+
+
+
+
+ 
+    await page.pause(); // Pause to observe the result
 });
