4bbc6472f39386b86592fe83707ee5b7944ee458
Rahman2Sazal <saz99man.99@gmail.com>
2026-09-12 17:38:45 +0200

    trying to make a project

--- tests/Chapter_2/06_mouseButtons.spec.ts
+++ tests/Chapter_2/06_mouseButtons.spec.ts
@@ -3,19 +3,25 @@ import {test, expect} from '@playwright/test';
 test ('test', async ({page}) => {
   await page.goto('https://www.youtube.com/');
 
-  const rejectButton= await page.getByRole('button', {name: /reject/i});
+  const rejectButton=page.getByRole('button', {name: /reject/i});
 
   if (await rejectButton.isVisible()) {
     await rejectButton.click();
     }
 
-    const searchBox= await page.getByRole('combobox', {name: /search/i});
+    const searchBox= page.getByRole('combobox', {name: /search/i});
     await expect(searchBox).toBeVisible();
     await searchBox.click();
-    await searchBox.fill('french b1 practice and podcast');
-    await searchBox.press('Enter');
+    await searchBox.fill('B1 french podcast playlist');
+    await page.getByRole('button', {name: 'Search', exact: true}).click();
+   // await searchBox.press('Enter');
 
+    await expect(page.getByText('B1 French Podcast').first()).toBeVisible();
+  
     await page.pause(); // Pause to observe the result
+
+    
+
 
    
 });
